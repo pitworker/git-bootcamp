243db9c84a1647f350eb7c9301b8d1075e8d5021
Sebastian Carpenter <scarpent@unix8.andrew.cmu.edu>
2019-01-20 19:55:05 -0500

    added comment

--- cprogramminglab-handout/queue.c
+++ cprogramminglab-handout/queue.c
@@ -1,4 +1,4 @@
-/* 
+/*
  * Code for basic C skills diagnostic.
  * Developed for courses 15-213/18-213/15-513 by R. E. Bryant, 2017
  * Modified to store strings, 2018
@@ -10,7 +10,7 @@
  *
  * It uses a singly-linked list to represent the set of queue elements
  */
-
+//change
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
@@ -109,4 +109,3 @@ void q_reverse(queue_t *q)
 {
     /* You need to write the code for this function */
 }
-
